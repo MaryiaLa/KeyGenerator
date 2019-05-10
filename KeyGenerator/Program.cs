1cc2bdc8de597584e4625916d3252fa064b23245
Maryia Lakhouskaya <Maryia_Lakhouskaya@epam.com>
2019-05-10 17:17:49 +0300

    commit

--- KeyGenerator/Program.cs
+++ KeyGenerator/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,15 @@ namespace KeyGenerator
     {
         static void Main(string[] args)
         {
+            byte[] dateBytes = BitConverter.GetBytes(DateTime.Now.Date.ToBinary());
+            NetworkInterface networkInterface = NetworkInterface.GetAllNetworkInterfaces().FirstOrDefault();
+            byte[] addressBytes = networkInterface.GetPhysicalAddress().GetAddressBytes();
+            int[] convertedAddressBytes = addressBytes.Select((x, index) => (x ^ dateBytes[index]) * 10).ToArray();
+            string result = string.Join("-", convertedAddressBytes);
+
+            Console.WriteLine("Cenerated key:");
+            Console.WriteLine(result);
+            Console.ReadLine();
         }
     }
 }
